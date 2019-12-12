feae4942f9a4b8a5e2aadf5dbdddf7fba8004865
Jip <jipfrijlink@gmail.com>
2019-12-12 19:15:24 +0100

    Verwijder test sockets

--- index.js
+++ index.js
@@ -3,8 +3,3 @@ require("dotenv").config();
 
 // const { client, handle_json, handle_message } = require("./tcp");
 const { app, server, io } = require("./server");
-
-// Om sockets te testen
-// setInterval(() => {
-//     io.emit("test", "Dit is een test bericht!");
-// }, 2e3);
