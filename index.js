e42a511d9f8d98250f37d29617e3c91a935b03d9
Jip <jipfrijlink@gmail.com>
2019-12-12 19:26:37 +0100

    Aantal JS bestanden in core mapje

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("colors");
 require("dotenv").config();
 
-// const { client, handle_json, handle_message } = require("./tcp");
-const { app, server, io } = require("./server");
+const { client, handle_json, handle_message } = require("./core/tcp");
+const { app, server, io } = require("./core/server");
