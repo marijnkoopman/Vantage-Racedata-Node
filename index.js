0c93e8c380a253b14740d5fb94ad8745e3850156
Jip F <jipfrijlink@gmail.com>
2019-12-13 09:27:54 +0100

    Variabelen weggehaald, die hadden geen nut...

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 require("colors");
 require("dotenv").config();
 
-const { client, handle_json, handle_message } = require("./core/tcp");
-const { app, server, io } = require("./core/server");
+require("./core/tcp");
+require("./core/server");
