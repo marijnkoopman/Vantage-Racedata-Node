a0e0401689e39d0d339c0ef947ce1fb9e534c8b5
marijnkoopman <marijnkoopman@gmail.com>
2020-01-16 11:15:47 +0100

    buttons toegevoegd

--- core/gpio.js
+++ core/gpio.js
@@ -1,14 +1,40 @@
 const gpio = require('onoff').Gpio;
-const buttonUp = new gpio(4, 'in', 'rising', {debounceTimeout: 10});
+const buttons = {
+	switchMode:		4,
+	lapIncrease:	17,
+	lapDecrease:	27,
+	ringBell:		22
+}
 
-buttonUp.watch((err, value) => {
-  if (err) {
-    throw err;
-  }
-  console.log('buttonUp =', value);
-});
+for(let key of Object.keys(buttons)) {
+	buttons[key] = new gpio(buttons[key], 'in', 'both', {debounceTimeout: 10});
+}
+
+button.switchMode.watch(switchMode);
+button.lapIncrease.watch(lapIncrease);
+button.lapDecrease.watch(lapDecrease);
+button.ringBell.watch(ringBell);
+
+function switchMode(err, value) {
+	if (err) throw err;
+	console.log('switchmode =', value);
+}
+
+function lapIncrease(err, value) {
+	if (err) throw err;
+	console.log('switchmode =', value);
+}
+
+function lapDecrease(err, value) {
+	if (err) throw err;
+	console.log('switchmode =', value);
+}
+
+function ringBell(err, value) {
+	if (err) throw err;
+	console.log('switchmode =', value);
+}
 
 process.on('SIGINT', _ => {
-  led.unexport();
-  button.unexport();
+	Object.values(buttons).forEach(b => b.unexport());
 });
