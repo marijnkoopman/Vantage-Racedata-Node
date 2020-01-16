e92f881f854242010a0ddb853290cd966aa1ae7d
marijnkoopman <marijnkoopman@gmail.com>
2020-01-16 09:53:08 +0000

    werkend gemaakt op de Pi

--- core/gpio.js
+++ core/gpio.js
@@ -1,13 +1,12 @@
 const gpio = require('onoff').Gpio;
 const buttonUp = new gpio(4, 'in', 'rising', {debounceTimeout: 10});
- 
+
 buttonUp.watch((err, value) => {
   if (err) {
     throw err;
   }
-  if (value = 1) console.log('buttonUp = 1');
-  if (value = 0) console.log('buttonUp = 0');
-}  
+  console.log('buttonUp =', value);
+});
 
 process.on('SIGINT', _ => {
   led.unexport();
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("colors");
 require("dotenv").config();
 
-require("./core/tcp");
+//require("./core/tcp");
 require("./core/server");
 require("./core/gpio");
